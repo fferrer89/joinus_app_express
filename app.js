530919d5f073c65eef1b327012ab89208ba9c438
fferrer89 <f.ferrer@me.com>
2021-02-11 10:07:23 -0600

    Commit 2

--- app.js
+++ app.js
@@ -1,41 +1,40 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+const mysql = require('mysql');
+const bodyParser = require("body-parser");
+const express = require('express');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+const app = express();
 
-var app = express();
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+let connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'yournewpassword',
+    database: 'joinus_db'
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+app.get("/", function (req, res) {
+    // Find count of users in DB
+    let q = "SELECT COUNT(*) AS count FROM users";
+    connection.query(q, function (err, results) {
+        if (err) throw err;
+        let count = results[0].count; // isolate the count from the results
+        res.render("home", {count: count});
+    });
 });
 
-module.exports = app;
+app.post("/register", function (req, res) {
+    let person = {
+        email: req.body.email
+    };
+    connection.query('INSERT INTO users SET ?', person, function (err, result) {
+        if (err) throw err;
+        res.redirect("/");
+    });
+});
+
+app.listen(8080, function () {
+    console.log("Server running on 8080!");
+});
